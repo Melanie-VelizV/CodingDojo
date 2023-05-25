e1850903debff0fdf85bca2d9f181da487235f7e
Melanie <45540145+Melanie-VelizV@users.noreply.github.com>
2023-05-25 00:44:14 -0400

    Add files via upload

---
+++ CODING DOJO PRACTICO/Pizzeria Dojo/index.js
@@ -0,0 +1,18 @@
+//Función que crea un objeto pizza
+function pizzaOven(tipoCoreteza,tipoSalsa,queso,salsas) {
+    var pizza = {};
+        pizza.tipoCoreteza = tipoCoreteza;
+        pizza.tipoSalsa = tipoSalsa;
+        pizza.queso = queso;
+        pizza.salsas = salsas;
+        return pizza;
+}
+
+var p1 = pizzaOven("estilo Chicago", "tradicional", ["mozzarella"], ["pepperoni", "salchicha"]);
+console.log(p1);
+
+var p2 = pizzaOven("lanzada a mano" , "marinara" , ["mozzarella", "feta"], ["champiñones", "aceitunas", "cebollas"]);
+console.log(p2);
+
+var p3 = pizzaOven("pan plano", "barbacoa casera", ["mozzarella"], ["pepperoni", "pollo", "champiñones"]);
+console.log(p3);
